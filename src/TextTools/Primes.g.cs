f2a5d2572a66b29d06a464b7ee563c8bb8bd8fc1
brian-reichle <18721383+brian-reichle@users.noreply.github.com>
2021-11-21 01:05:51 +1000

    Implemented GetPrime.

---
+++ src/TextTools/Primes.g.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Brian Reichle.  All Rights Reserved.  Licensed under the MIT License.  See License.txt in the project root for license information.
+namespace TextTools
+{
+	static partial class Primes
+	{
+		const int MaxPrime = 2147483647;
+
+		static readonly int[] _primes =
+		{
+			3, 5, 7, 11, 15, 23, 31, 47, 67, 97, 101, 137, 197, 211, 277, 397, 431,
+			557, 797, 863, 1117, 1597, 1733, 2237, 3203, 3467, 4481, 6421, 6947, 8963,
+			12853, 13901, 17929, 25717, 27803, 35863, 51437, 55609, 71741, 102877,
+			111227, 143483, 205759, 222461, 286973, 411527, 444929, 573953, 823117,
+			889871,
+		};
+	}
+}
